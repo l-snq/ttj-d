a6117cf649ba7107e984d95b57bae08e8b4ea617
ttj <tastytothesupreme@gmail.com>
2024-02-24 20:07:56 -0700

    idk what i was doing lol

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,17 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.image}> hey </div>
-      <div>
-      //////////////////////////////////////////////////////////////////////////////
-      </div>
-      <h1>Hi. My name is ttj//d. Welcome to my site.</h1>
-      <div className={styles.description}>
-        <p>
-            I am a programmer, modder and overall I love to tinker with things.
-          I've been programming since i was 14 years old. What started as just making mods for a game quickly grew into an obsession with building software.
-          <a href="https://github.com/tastetest/">My portfolio</a>
-        </p>
-      </div>
+        <div>
+        //////////////////////////////////////////////////////////////////////////////
+        </div>
+        <h1>Hi. My name is ttj//d. Welcome to my site.</h1>
+        <div className={styles.description}>
+          <p>
+              I am a programmer, modder and overall I love to tinker with things.
+            I've been programming since i was 14 years old. What started as just making mods for a game quickly grew into an obsession with building software.
+            <a href="https://github.com/tastetest/">My portfolio</a>
+          </p>
+        </div>
     </main>
   );
 }
