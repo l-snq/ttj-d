6ba4481cd3ca898dd6cb1cdb7f77c51578671453
ttj <tastytothesupreme@gmail.com>
2024-02-24 20:19:59 -0700

    fixed some build issues

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <main className={styles.main}>
         <div>
-        //////////////////////////////////////////////////////////////////////////////
+          *****************************************************************************
         </div>
-        <h1>Hi. My name is ttj//d. Welcome to my site.</h1>
+        <h1>Hi. My name is ttjd. Welcome to my site.</h1>
         <div className={styles.description}>
           <p>
               I am a programmer, modder and overall I love to tinker with things.
-            I've been programming since i was 14 years old. What started as just making mods for a game quickly grew into an obsession with building software.
-            <a href="https://github.com/tastetest/">My portfolio</a>
+            I have been programming since i was 14 years old. What started as just making mods for a game quickly grew into an obsession with building software.
+            <a href={'https://github.com/tastetest/'}>My portfolio</a>
           </p>
         </div>
     </main>
