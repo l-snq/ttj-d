830985e00b4693e2590fb6212b0de9d87d06188f
ttj <tastytothesupreme@gmail.com>
2024-02-24 19:30:56 -0700

    added to the website :3

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div>
+      //////////////////////////////////////////////////////////////////////////////
+      </div>
+      <h1>Hi. My name is ttj//d. Welcome to my site.</h1>
       <div className={styles.description}>
-        <p>hi whats up</p>
+        <p>
+            I am a programmer, modder and overall I love to tinker with things.
+          I've been programming since i was 14 years old. What started as just making mods for a game quickly grew into an obsession with building software.
+          <a href="https://github.com/tastetest/">My portfolio</a>
+        </p>
       </div>
     </main>
   );
