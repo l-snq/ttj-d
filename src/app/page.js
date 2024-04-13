901bb0bac73233b5ceec4143bf953e0edd7e101e
ttjdesktop <tastytothesupreme@gmail.com>
2024-04-13 12:51:18 -0600

    something

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-        <div>
-          *****************************************************************************
+	<div className={styles.description}>
+		<h1>Hi. My name is ttjd. Welcome to my site.</h1>
         </div>
-        <h1>Hi. My name is ttjd. Welcome to my site.</h1>
         <div className={styles.description}>
           <p>
               I am a programmer, modder and overall I love to tinker with things.
