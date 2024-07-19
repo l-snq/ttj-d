a3efb8127ad93b8d8faf3f2badb6883a200a3811
ttjdesktop <tastytothesupreme@gmail.com>
2024-07-19 15:48:23 -0600

    cleaned up stuff, updated links and changed the wordings, as well as some of the styling behaviours. Just a general tidy up!

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,19 @@ export default function Home() {
   return (
     <main className={styles.main}>
 	<div className={styles.description}>
-		<h1>Hi. My name is ttjd. Welcome to my site.</h1>
+		<h1>Hi. My name is lsnq. Welcome to my site.</h1>
+		  <p>
+		      I am a programmer, modder and overall I love to tinker with things.
+		    I have been programming since i was 14 years old. What started as just making mods for a game quickly grew into an obsession with building software.
+		    <a href={'https://github.com/l-snq/'}> 🤠 My portfolio 🤠</a>
+		  </p>
+		  <p>
+	  		Feel free to check out my latest project an app launcher for wlroots based compositors in linux:
+	  		<a className={styles.project} href={'https://github.com/l-snq/tau/'}>👉 Tau 👈</a>
+	  		
+		  </p>
         </div>
         <div className={styles.description}>
-          <p>
-              I am a programmer, modder and overall I love to tinker with things.
-            I have been programming since i was 14 years old. What started as just making mods for a game quickly grew into an obsession with building software.
-            <a href={'https://github.com/tastetest/'}>My portfolio</a>
-          </p>
         </div>
     </main>
   );
