2932a575ecb2c302f4ebc865d317c7a7a631c47b
ttjdesktop <tastytothesupreme@gmail.com>
2024-07-19 15:51:41 -0600

    updated metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ttj",
+  title: "lsnq",
   description: "programmer, software developer, tech enthusiast",
 };
 
